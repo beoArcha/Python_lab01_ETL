d77d1f36553e6c2cf0f138dff3f7a1609f7c5d1a
beoArcha <MilkWAY88>
2019-11-01 13:30:05 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Python_lab01_ETL',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='MIT',
+    author='Marcin Stefanski',
+    author_email='marcinstefanski@outlook.com',
+    description='for learning purposes'
+)
